7c7c6e8271a817637dafda66d1d2a50193247b07
zerocool <exwarvlad@gmail.com>
2017-02-08 01:27:31 +0200

    add profile user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
     @user = current_user.build(user_params)
   end
 
+  def show
+    @user = User.find(params[:id])
+
+    # все книги пользователя
+    @books = Book.all.where(user: @user)
+  end
+
   private
 
   def user_params
