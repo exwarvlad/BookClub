73b03afd4023c1c1ba36320e284fd7b0c41ccdca
Zero_Cool <exwarvlad@gmail.com>
2017-02-18 21:12:04 +0000

    fixed authenticate to omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable, :omniauthable
+          :rememberable, :trackable, :validatable, :omniauthable
 
 
   has_many :books
@@ -16,19 +16,9 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.email = nil
       user.name = auth.info.name
-      user.password = Devise.friendly_token[0, 20]
-
-      user.email = auth.info.email
-
-      # если email не уникальный или его нету, то генерю случайный
-      unless user.valid?
-        count = 0
-        while user.valid? == false || count != 42
-          user.email = "#{rand(99999999999999)}@#{auth.provider}"
-          count += 1
-        end
-      end
+      user.password = Devise.friendly_token[0,20]
     end
   end
 end
