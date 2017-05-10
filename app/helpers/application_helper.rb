c9801b09c4e7ca3867a6fb5673500c9c452d0987
zerocool <exwarvlad@gmail.com>
2017-05-10 20:02:17 +0300

    add comments vision

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_last_comments(condition, comments)
+    if condition
+      content_tag(:div, content_tag(:p, "No comment"), class: "caption")
+    else
+      content_tag(:div, class: "caption") { comments.collect {|item| concat(content_tag(:p, truncate(item, length: 29)))} }
+    end
+  end
 end
